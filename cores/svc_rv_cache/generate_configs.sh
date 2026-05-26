64422f3da0ba47255854a6b1c95a4f8aba9803d8
Patrick Bozeman <pbozeman@gmail.com>
2026-05-26 01:45:39 -0700

    include new misc dir

--- cores/svc_rv_cache/generate_configs.sh
+++ cores/svc_rv_cache/generate_configs.sh
@@ -140,6 +140,7 @@ verilog_defaults -add -I@basedir@/../../rtl/common
 verilog_defaults -add -I@basedir@/../../rtl/axi
 verilog_defaults -add -I@basedir@/../../rtl/fifo
 verilog_defaults -add -I@basedir@/../../rtl/rv
+verilog_defaults -add -I@basedir@/../../src/misc/rtl
 
 [defines]
 \`define RISCV_FORMAL_ALIGNED_MEM
